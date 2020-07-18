acc1fa692c70e02b9f4de48ef09920653de10187
沈构强 <shengouqiang@Shen-LocalPC.lan>
2020-07-18 13:34:22 +0800

    add

--- leet-code/src/main/java/leetcode/no33/Solution.java
+++ leet-code/src/main/java/leetcode/no33/Solution.java
@@ -1,27 +1,29 @@
 package leetcode.no33;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * 33. 搜索旋转排序数组
  * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
- *
+ * <p>
  * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
- *
+ * <p>
  * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
- *
+ * <p>
  * 你可以假设数组中不存在重复的元素。
- *
+ * <p>
  * 你的算法时间复杂度必须是 O(log n) 级别。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: nums = [4,5,6,7,0,1,2], target = 0
  * 输出: 4
  * 示例 2:
- *
+ * <p>
  * 输入: nums = [4,5,6,7,0,1,2], target = 3
  * 输出: -1
  * 通过次数131,906提交次数346,457
- *
  *
  * @author shengouqiang
  * @date 2020/6/17
@@ -29,29 +31,67 @@ package leetcode.no33;
 public class Solution {
 
 
-    /**
-     * 自己的解题思路
-     *  1.首先根据二分查找，找到数组中的最大值
-     *  2.以最大值为基点，将数组分成两个单调增的区分
-     *  3.在每个单调增的区间，在进行二分查找
-     *  4.获取最终的返回值
-     *
-     * @param nums
-     * @param target
-     * @return
-     */
-    public int search(int[] nums, int target) {
-        return -1;
+    @Test
+    public void testOne() {
+        Assert.assertEquals(4, search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
     }
 
-    private int findMaxValueIndexByBinarySearch(int[] array){
-        int left = 0;
-        int right = array.length -1;
-        int med = (left +right) /2;
-        while (left < right){
+    @Test
+    public void testTwo() {
+        Assert.assertEquals(-1, search(new int[]{4, 5, 6, 7, 0, 1, 2}, 3));
+    }
 
+    @Test
+    public void testThree() {
+        Assert.assertEquals(1, search(new int[]{8,9,2,3,4}, 9));
+    }
+
+
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length < 1) {
+            return -1;
+        }
+        int length = nums.length;
+        if (nums[0] < nums[length - 1]) {
+            return findValue(nums, 0, length - 1, target);
+        } else {
+            int maxValueIndex = findMaxIndex(nums, 0, length - 1);
+            int tempA = findValue(nums, 0, maxValueIndex, target);
+            int tempB = findValue(nums, maxValueIndex + 1, length - 1, target);
+            if (tempA == -1 && tempB == -1) {
+                return -1;
+            }
+            return tempA == -1 ? tempB : tempA;
         }
 
-        return 0;
+    }
+
+    public int findMaxIndex(int[] nums, int start, int end) {
+        int maxValueIndex = start;
+        while (start <= end) {
+            int med = start + ((end - start) >>> 1);
+            if (nums[med] < nums[maxValueIndex]) {
+                end = med - 1;
+            } else {
+                start = med + 1;
+                maxValueIndex = med;
+            }
+        }
+        return maxValueIndex;
+    }
+
+    public int findValue(int[] nums, int start, int end, int target) {
+        while (start <= end) {
+            int med = start + ((end - start) >>> 1);
+            if (nums[med] == target) {
+                return med;
+            }
+            if (nums[med] < target) {
+                start = med + 1;
+            } else {
+                end = med - 1;
+            }
+        }
+        return -1;
     }
 }
