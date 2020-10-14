1ba92d7406f5f519b391830529d49f9b9b9fb49f
沈构强 <shengouqiang@iMac-Pro.lan>
2020-10-14 22:56:20 +0800

    add

--- springboot-test/src/test/java/Test.java
+++ springboot-test/src/test/java/Test.java
@@ -1,6 +1,17 @@
 public class Test {
 
-    public synchronized  void show(){
-
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,3,2,4,3,5};
+        int i=1;
+        int j=4;
+        System.out.println("----交换前-----");
+        System.out.println(arr[i]);
+        System.out.println(arr[j]);
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+        System.out.println("----交换后-----");
+        System.out.println(arr[i]);
+        System.out.println(arr[j]);
     }
 }
